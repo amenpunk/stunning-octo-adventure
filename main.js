8fff6445c1be9890c2919ffec1be0b498a4cbfb6
amenpunk <ondasycircuitos@gmail.com>
2019-09-26 13:05:26 -0600

    konami :ok_hand:

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+const bus = ["u","m","g"];
+
 window.onload = function () {
 
     var letras = ['a', 'b', 'c', 'd', "e", 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', "n", "ñ", 'o', 'p', 'k', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -42,10 +45,22 @@ window.onload = function () {
     }
 
     var cont = 0;
+    var index =0;
+
     lo = document.getElementsByTagName('button')
     while (cont < letras.length) {
         lo[cont].onclick = function () {
-            console.log(this.name);
+            factor = this.name;
+            console.log(factor);
+            if(factor === bus[index] ){
+                index++;
+                if(index === bus.length){
+                    alert("ENCONTRASTE UMG !!");
+                }
+            }else{
+                index=0;
+            }
+            
         }
         cont++;
     }
